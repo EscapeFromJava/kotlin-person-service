940f5a2303ff103be7308dfc119e76633e484915
EscapeFromJava <reske.artur@gmail.com>
2023-05-21 23:28:36 +0300

    remove useless properties add constraints for Person

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,3 @@
-#FROM bellsoft/liberica-openjdk-alpine
-#ENV APP_HOME=/app
-#ENV POSTGRES_URL=jdbc:postgresql://localhost:5432/test
-#ENV POSTGRES_USER=postgres
-#ENV POSTGRES_PASSWORD=bestuser
-#RUN mkdir $APP_HOME
-#COPY . $APP_HOME
-#WORKDIR $APP_HOME
-#RUN ./mvnw clean package
-#EXPOSE 8080
-#CMD ["java", "-jar", "target/*.jar"]
-
 FROM bellsoft/liberica-openjdk-alpine
 COPY target/*.jar app.jar
-EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/kotlin/com/example/kotlinpersonservice/model/entity/Person.kt
+++ src/main/kotlin/com/example/kotlinpersonservice/model/entity/Person.kt
@@ -1,9 +1,6 @@
 package com.example.kotlinpersonservice.model.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 data class Person(
@@ -11,7 +8,9 @@ data class Person(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
+    @Column(unique = true, nullable = false)
     var name: String?,
+    @Column(unique = true, nullable = false)
     var email: String?
 
 )
